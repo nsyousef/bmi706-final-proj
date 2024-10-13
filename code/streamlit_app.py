0fe6ab93275bea29b77d55cbe3ba1e9a9d350b45
Nicholas Yousefi <nsyousef@ucsd.edu>
2024-10-13 00:56:37 -0400

    add more to template

--- code/streamlit_app.py
+++ code/streamlit_app.py
@@ -2,8 +2,20 @@ import streamlit as st
 import pandas as pd
 import altair as alt
 
-# load in the data
-anxiety_depression_data = pd.read_csv("data/indicators_anxiety_depression.csv")
-death_counts_data = pd.read_csv("data/death_counts.csv")
+# load and wrangle the data
+@st.cache_data
+def load_data():
+    anxiety_depression_data = pd.read_csv("data/indicators_anxiety_depression.csv")
+    death_counts_data = pd.read_csv("data/death_counts.csv")
+    return anxiety_depression_data, death_counts_data
 
-st.write("# Our BMI706 Project", unsafe_allow_html=True)
+anxiety_depression_data, death_counts_data = load_data()
+
+st.write("# Anxiety and Depression 2020 - 2024")
+
+# mental health symptoms by category
+st.write("## Mental Health Symptoms by Category")
+
+st.write("## Mental Health Symptoms By State")
+
+st.write("## Deaths of Despair and Mental Health Symptoms")
